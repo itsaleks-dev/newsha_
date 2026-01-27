8cd2bb61a1d7141e930676a5550ce1e06e174765
Admin <admin@MacBook-Pro.local>
2026-01-27 02:29:51 +0200

    setup project infrastructure

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import importPlugin from "eslint-plugin-import";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist", "coverage", "node_modules"]),
+
+  {
+    files: ["**/*.{ts,tsx}"],
+
+    plugins: {
+      import: importPlugin,
+    },
+
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+    },
+
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+
+    rules: {
+      quotes: ["error", "double", { avoidEscape: true }],
+
+      "import/no-restricted-paths": [
+        "error",
+        {
+          zones: [
+            { target: "./src/app", from: ["./src/features", "./src/entities", "./src/shared"] },
+            { target: "./src/entities", from: ["./src/shared", "./src/widgets", "./src/pages"] },
+            { target: "./src/shared", from: ["./src/entities", "./src/features", "./src/widgets"] },
+          ],
+        },
+      ],
+
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: ["@/features/*", "@/entities/*", "@/shared/*"],
+        },
+      ],
+    },
+  },
+]);
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@types": path.resolve(__dirname, "./src/types"),
+    },
+  },
+});
